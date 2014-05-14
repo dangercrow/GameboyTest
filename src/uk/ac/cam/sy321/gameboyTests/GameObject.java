e47de8f7cad17be6aac231441b94235e33a8298f
dangercrow <dangercrow@gmail.com>
2014-05-14 15:53:56 +0100

    GameObject fillers
    Abstracted interactions to a new class
    Added blank override methods in inherited classes

--- src/uk/ac/cam/sy321/gameboyTests/GameObject.java
+++ src/uk/ac/cam/sy321/gameboyTests/GameObject.java
@@ -2,4 +2,42 @@ package uk.ac.cam.sy321.gameboyTests;
 
 public class GameObject {
 
+	private char Repr='x';
+	private Interaction interaction;
+
+
+
+	public GameObject(Interaction interact, char Representation){
+		Repr=Representation;
+		interaction = interact;
+	}
+
+	public GameObject(Interaction interact){
+		interaction = interact;
+	}
+
+	public GameObject(char Representation){
+		Repr=Representation;
+		interaction = new Interaction();
+	}
+
+	public GameObject(){
+		interaction = new Interaction();
+	}
+
+
+
+	public char getRepr() {
+		return Repr;
+	}
+
+	public Interaction getInteraction() {
+		return interaction;
+	}
+
+
+
+	public void onInteract(){
+
+	}
 }
--- src/uk/ac/cam/sy321/gameboyTests/LockedDoor.java
+++ src/uk/ac/cam/sy321/gameboyTests/LockedDoor.java
@@ -1,5 +1,8 @@
 package uk.ac.cam.sy321.gameboyTests;
 
 public class LockedDoor extends GameObject{
-
+	@Override
+	public void onInteract(){
+		
+	}
 }
