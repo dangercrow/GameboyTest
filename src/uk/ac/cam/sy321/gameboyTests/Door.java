79cc37e2420067048e0008b971475843f21cf98c
dangercrow <dangercrow@gmail.com>
2014-05-14 16:39:46 +0100

    Door basics
    Extended constructor, getters and setters

--- src/uk/ac/cam/sy321/gameboyTests/Door.java
+++ src/uk/ac/cam/sy321/gameboyTests/Door.java
@@ -1,7 +1,22 @@
 package uk.ac.cam.sy321.gameboyTests;
 
 public class Door extends Tile {
-	public Door(){
+	
+	private char repr='-';
+	
+	public Door(boolean locked){
 		super();
+		if (locked){
+			super.setObject(new LockedDoor());
+		}
+	}
+	
+	@Override
+	public char getRepr(){
+		return (super.hasObject() ? super.getObject().getRepr() : repr);
+	}
+	
+	public void setRepr(char representation){
+		repr=representation;
 	}
 }
